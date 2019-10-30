79b4c53cb6a3c5023360726e54456f9e66e7fd1f
husnutapan <husnu.tapan@gmail.com>
2019-10-30 21:51:36 +0300

    create parent project

---
+++ concurrencies/src/main/java/com/java/efficent/ProducerAndConsumerThread.java
@@ -0,0 +1,95 @@
+package com.java.efficent;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class ProducerAndConsumerThread {
+
+    public static void main(String args[]) {
+        Queue sharedQueue = new LinkedList();
+        int size = 15;
+        Thread prodThread = new Thread(new Producer(sharedQueue, size), "Producer");
+        Thread consThread = new Thread(new Consumer(sharedQueue), "Consumer");
+        prodThread.start();
+        consThread.start();
+    }
+}
+
+class Producer implements Runnable {
+
+    private final Queue sharedQueue;
+    private final int SIZE;
+
+    public Producer(Queue sharedQueue, int size) {
+        this.sharedQueue = sharedQueue;
+        this.SIZE = size;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 7; i++) {
+            System.out.println("Produced: " + i);
+            try {
+                produce(i);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Producer.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+        }
+    }
+
+    private void produce(int i) throws InterruptedException {
+        while (sharedQueue.size() == SIZE) {
+            synchronized (sharedQueue) {
+                System.out.println("Queue is full " + Thread.currentThread().getName()
+                        + " is waiting , size: " + sharedQueue.size());
+
+                sharedQueue.wait();
+            }
+        }
+
+        synchronized (sharedQueue) {
+            sharedQueue.add(i);
+            sharedQueue.notifyAll();
+        }
+    }
+}
+
+class Consumer implements Runnable {
+
+    private final Queue sharedQueue;
+
+    public Consumer(Queue sharedQueue) {
+        this.sharedQueue = sharedQueue;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                System.out.println("Consumed: " + consume());
+                Thread.sleep(50);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Consumer.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+
+    private int consume() throws InterruptedException {
+        while (sharedQueue.isEmpty()) {
+            synchronized (sharedQueue) {
+                System.out.println("Queue is empty " + Thread.currentThread().getName()
+                        + " is waiting , size: " + sharedQueue.size());
+
+                sharedQueue.wait();
+            }
+        }
+
+        synchronized (sharedQueue) {
+            sharedQueue.notifyAll();
+            return (Integer) sharedQueue.remove();
+        }
+    }
+}
